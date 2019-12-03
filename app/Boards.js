424d9822e76c02b6f8591da57e15984d0f612e44
mathiashertzenberg <mahe19ac@student.cbs.dk>
2019-12-03 16:48:27 +0100

    skrt

--- app/Boards.js
+++ app/Boards.js
@@ -1,5 +1,12 @@
-Class Boards {
-    constructor(type) {
-        this.type=type;
-    }
+
+
+function myFunction() {
+
+    var board = document.getElementById("minSelectBoard").value;
+    var dag = document.getElementById("minSelectDag").value;
+    var month= document.getElementById("minSelectMonth").value;
+    var year=document.getElementById("minSelectYear").value;
+
+    alert("Du har booket " + board + " den " + dag + " " + month + " " + year);
 }
+
