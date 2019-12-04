c8c0d478778ef7fc8c4aa64452e67559c35dd2ab
mathiashertzenberg <mahe19ac@student.cbs.dk>
2019-12-04 14:19:19 +0100

    skrt

--- app/Boards.js
+++ app/Boards.js
@@ -10,3 +10,16 @@ function myFunction() {
     alert("Du har booket " + board + " den " + dag + " " + month + " " + year);
 }
 
+var items = [{name:"board 1"}, {name:"board 2"}, {name: "board 3"}, {name: "board 4"}]
+
+
+//window.localStorage.setItem("items", items)
+
+function checkAvailability(){
+    for (item in window.localStorage.getItem("items")){
+        if (window.localStorage.getItem(item.name)){
+            alert ("board is booked")}
+        else {window.localStorage.setItem("items", items)}
+        alert ("available")
+    }
+}
