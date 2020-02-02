ac33f8011b43091cecf784258a1533f0b9746b94
mcnolsen <miol19ad@student.cbs.dk>
2020-02-02 12:36:59 +0100

    Tilfoejet mulighed for at skifte password i profil undersiden for user. Viser også email og username for den bruger der er logget ind

--- routes/user/user-routes.js
+++ routes/user/user-routes.js
@@ -4,6 +4,8 @@ const auth = require('../../config/auth');
 const Board = require('../../models/Board');
 const BookedBoard = require('../../models/BookedBoards');
 const sorting = require('./app/sortFilter');
+const User = require('../../models/User');
+const bcrypt = require ('bcryptjs');
 
 
 //User routes
@@ -28,13 +30,44 @@ app.get('/user/logout', auth.checkAuthentification, (req, res) => {
     res.render('./user/logout.ejs')
 })
 app.get('/user/profil', auth.checkAuthentification, (req, res) => {
-    res.render('./user/profil.ejs', {userType: req.user.userType})
+    res.render('./user/profil.ejs', {userType: req.user.userType, username: req.user.username, email: req.user.email})
 })
 app.get('/user/reglement', auth.checkAuthentification, (req, res) => {
     res.render('./user/reglement.ejs', {userType: req.user.userType})
 })
 app.get('/user/dashboard', auth.checkAuthentification, (req, res) => {
     res.render('./user/dashboard.ejs', {username: req.user.username, userType: req.user.userType});
+})
+
+//Skift password route
+app.post('/user/changePassword', auth.checkAuthentification, async (req, res) => {
+    const newPassword = req.body.newPassword;
+    const newPasswordConfirm = req.body.newPasswordConfirm;
+    //Forskellige password checks
+    try {
+        if (newPassword === ""){
+            req.flash('error', 'Venligst indtast et password');
+            res.redirect('./profil');
+        }
+        else if (newPassword !== newPasswordConfirm){
+            req.flash('error', 'De 2 indtastede passwords matcher ikke');
+            res.redirect('./profil');
+        }
+        else if (newPassword.length < 8){
+            req.flash('error', 'Vælg venligst et password på over 8 tegn');
+            res.redirect('./profil');
+        }
+        else {
+            const newPasswordHash = await bcrypt.hash(newPassword, 10);
+            console.log(req.user._id);
+            await User.updateOne({'_id': req.user._id}, {'password': newPasswordHash});
+            req.flash('success', 'Dit password er nu blevet skiftet');
+            res.redirect('./profil');
+        }
+    } 
+    catch (err) {
+        console.log(err)
+    }
 })
 
 module.exports = app;
